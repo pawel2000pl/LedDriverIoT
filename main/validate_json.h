ab1473493ce9a45ed30e7d702f66e0b22ac86c44
Paweł Bielecki <pawelek24@op.pl>
2024-05-04 11:17:56 +0200

    Json validation improovements

--- main/validate_json.h
+++ main/validate_json.h
@@ -16,52 +16,77 @@ bool validateRange(const JsonVariantConst& object, const JsonVariantConst& schem
   return 1;
 }
 
+const char* json_simple_types[] = {"object", "array", "boolean", "int", "integer", "float", "bool", "string", NULL};
 
-String validateJson(const JsonVariantConst& object, const JsonVariantConst& schema, String path=".") {
+bool isSimpleJsonType(String typeName) {
+  const char** iterator = json_simple_types;
+  while (*iterator) {
+    if (typeName == *iterator)
+      return true;
+    iterator++;
+  }
+  return false;
+}
+
+StaticJsonDocument<64> JsonEmpty;
+bool loadJsonEmpty() {
+  String emptyJson = "{}";
+  deserializeJson(JsonEmpty, emptyJson);
+  return 1;
+}
+bool JsonEmptyLoaded = loadJsonEmpty();
+
+String validateJson(const JsonVariantConst& object, const JsonVariantConst& schema, const JsonVariantConst& objectType, String path=".") {
+
+  bool objectTypeIsInline = objectType.is<String>();
+
+  if (objectTypeIsInline && !isSimpleJsonType(objectType.as<String>()))
+    return validateJson(object, schema, schema[objectType.as<String>()], path);
   
-  String type = schema.containsKey("type") ? schema["type"].as<JsonString>().c_str() : "object";
+  const JsonVariantConst& objectSchema = objectTypeIsInline ? JsonEmpty.as<JsonVariantConst>() : objectType;
+  String type = objectTypeIsInline ? objectType : objectSchema.containsKey("type") ? objectSchema["type"] : String("object");
 
   if (type == "object") {
     if (!object.is<JsonObjectConst>()) return "Invalid type: " + path + " expected: object";
-    const auto& fields = schema["fields"].as<JsonObjectConst>();
+    const auto& fields = objectSchema["fields"].as<JsonObjectConst>();
     for (const JsonPairConst& keyValue: fields) {
       const auto& key = keyValue.key();
       if (!object.containsKey(key)) return "Missing key: " + path + "/" + key.c_str();
-      String result = validateJson(object[key], keyValue.value(), path+"/"+key.c_str());
+      String result = validateJson(object[key], schema, keyValue.value(), path+"/"+key.c_str());
       if (result.length() != 0) return result;
     }
   }
 
   if (type == "array") {
     if (!object.is<JsonArrayConst>()) return "Invalid type: " + path + " expected: array";
-    const auto& item = schema["item"];
+    const auto& item = objectSchema["item"];
     const int size = object.size();
-    if (schema.containsKey("max_length") && size > schema["max_length"]) return "Array too long: " + path;
-    if (schema.containsKey("min_length") && size < schema["min_length"]) return "Array too short: " + path;
-    if (schema.containsKey("length") && size != schema["length"]) return "Invalid array size: " + path;
+    if (objectSchema.containsKey("max_length") && size > objectSchema["max_length"]) return "Array too long: " + path;
+    if (objectSchema.containsKey("min_length") && size < objectSchema["min_length"]) return "Array too short: " + path;
+    if (objectSchema.containsKey("length") && size != objectSchema["length"]) return "Invalid array size: " + path;
     for (int i=0;i<size;i++) {
-      String result = validateJson(object[i], item, path+"/"+inttostr(i));
+      String result = validateJson(object[i], schema, item, path+"/"+inttostr(i));
       if (result.length() != 0) return result;
     }
   }
 
   if (type == "float") {
     if (!object.is<float>()) return "Invalid type: " + path + " expected: float";
-    if (!validateRange(object, schema)) return "Invalid range: " + path;
+    if (!validateRange(object, objectSchema)) return "Invalid range: " + path;
   }
   if (type == "integer") {
     if (!object.is<int>()) return "Invalid type: " + path + " expected: integer";
-    if (!validateRange(object, schema)) return "Invalid range: " + path + "/" + path;
+    if (!validateRange(object, objectSchema)) return "Invalid range: " + path + "/" + path;
   }
   if (type == "bool" || type == "boolean") {
     if (!object.is<bool>()) return "Invalid type: " + path + " expected: boolean";
   }
   if (type == "string") {
     if (!object.is<const char*>()) return "Invalid type: " + path + " expected: string";
-    if (schema.containsKey("max_length") && object.size() > schema["max_length"]) return "String too long: " + path;
-    if (schema.containsKey("regexp") || schema.containsKey("regex")) {
+    if (objectSchema.containsKey("max_length") && object.size() > objectSchema["max_length"]) return "String too long: " + path;
+    if (objectSchema.containsKey("regexp") || objectSchema.containsKey("regex")) {
       String testString = object.as<String>();
-      String pattern = schema.containsKey("regexp") ? schema["regexp"].as<String>() : schema["regex"].as<String>();
+      String pattern = objectSchema.containsKey("regexp") ? objectSchema["regexp"].as<String>() : objectSchema["regex"].as<String>();
       regex_t restrict;
       regcomp(&restrict, pattern.c_str(), REG_EXTENDED | REG_NOSUB);      
       int result = regexec(&restrict, testString.c_str(), 0, NULL, 0);
@@ -76,3 +101,6 @@ String validateJson(const JsonVariantConst& object, const JsonVariantConst& sche
   return "";
 }
 
+String validateJson(const JsonVariantConst& object, const JsonVariantConst& schema) {
+  return validateJson(object, schema, schema["main"]);
+}
