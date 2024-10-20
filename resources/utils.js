12901382a49f4deddc73b5d2be5342e9c537ca10
Paweł Bielecki <pawelek24@op.pl>
2024-10-20 18:41:30 +0200

    Fixed menu touching

--- resources/utils.js
+++ resources/utils.js
@@ -4,10 +4,8 @@
 Array.from(document.getElementsByClassName('link-button')).forEach((button)=>{
     const linkAttr = button.attributes['link'] ?? undefined;
     const link = (linkAttr === undefined) ? './#' : linkAttr.value;
-    button.addEventListener('mouseup', e => {
-        if (e.button === 0) {
-            window.location = link;
-        }
+    button.addEventListener('click', _ => {
+        window.location = link;
     });
     button.addEventListener('mousedown', e => {
         if (e.button === 1) {
@@ -19,12 +17,15 @@ Array.from(document.getElementsByClassName('link-button')).forEach((button)=>{
     button.addEventListener('touchstart', e => {
         e.preventDefault();
         button.touchTimer = setTimeout(() => {
+            button.touchTimer = -1;
             window.open(link, '_blank');
         }, 500);
     });
     button.addEventListener('touchend', e => {
+        if (button.touchTimer < 0) return;
         clearTimeout(button.touchTimer);
         button.touchTimer = -1;
+        window.location = link;
     });
 });
 
