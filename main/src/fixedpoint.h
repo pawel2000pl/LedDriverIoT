cf8f79f09629f7d171585be655e2309b31d6d356
Paweł Bielecki <pawelek24@op.pl>
2025-01-26 00:17:17 +0100

    Updated fixed point

--- main/src/fixedpoint.h
+++ main/src/fixedpoint.h
@@ -6,8 +6,9 @@
 // T - type for data storage
 // TB - type used for multiplication and division
 // fracBits - count of fraction bits (bits after point)
+// simpleMultiplication - multiplication with the T type instad of the TB
 
-template<typename T, typename TB, unsigned fracBits>
+template<typename T, typename TB, unsigned fracBits, bool simpleMultiplication=(sizeof(T)==sizeof(TB))>
 class FixedPoint {
 
     public:
@@ -16,28 +17,26 @@ class FixedPoint {
         #define FIXED_POINT_FLOAT_TEMPLATE template<typename FP, typename std::enable_if<std::is_floating_point<FP>::value, FP>::type* = nullptr>
 
         FIXED_POINT_INTEGRAL_TEMPLATE
-        FixedPoint(const I value) : FixedPoint(value << fracBits, true) {}
+        FixedPoint(const I value) : FixedPoint((T)value << fracBits, true) {}
 
         FIXED_POINT_FLOAT_TEMPLATE
-        FixedPoint(const FP value) : FixedPoint(value * (1 << fracBits), true) {}
+        FixedPoint(const FP value) : FixedPoint(value * ((T)1 << fracBits), true) {}
 
         FixedPoint() : FixedPoint(0, true) {}
 
         FixedPoint(const FixedPoint& another) : FixedPoint(another.buf, true) {}
 
-        template<typename T2, typename TB2> 
+        template<typename T2, typename TB2>
         FixedPoint(const FixedPoint<T2, TB2, fracBits>& another)
          : buf(another.getBuf()) {}
 
-        template<typename T2, typename TB2, unsigned fracBits2> 
+        template<typename T2, typename TB2, unsigned fracBits2>
         FixedPoint(const FixedPoint<T2, TB2, fracBits2>& another)
          : buf(another.getBuf() << (int)((int)fracBits - (int)fracBits2)) {}
-
 
         friend FixedPoint operator+(const FixedPoint first, const FixedPoint second) {
             return FixedPoint(first.buf + second.buf, true);
         }
-
 
         FIXED_POINT_INTEGRAL_TEMPLATE
         friend FixedPoint operator+(const FixedPoint first, const I second) {
@@ -51,12 +50,12 @@ class FixedPoint {
 
         FIXED_POINT_FLOAT_TEMPLATE
         friend FixedPoint operator+(const FixedPoint first, const FP second) {
-            return FixedPoint(first.buf + second * (1 << fracBits), true);
+            return FixedPoint(first.buf + second * ((T)1 << fracBits), true);
         }
 
         FIXED_POINT_FLOAT_TEMPLATE
         friend FixedPoint operator+(const FP first, const FixedPoint second) {
-            return FixedPoint(first * (1 << fracBits) + second.buf, true);
+            return FixedPoint(first * ((T)1 << fracBits) + second.buf, true);
         }
 
         friend FixedPoint operator-(const FixedPoint first, const FixedPoint second) {
@@ -75,16 +74,19 @@ class FixedPoint {
 
         FIXED_POINT_FLOAT_TEMPLATE
         friend FixedPoint operator-(const FixedPoint first, const FP second) {
-            return FixedPoint(first.buf - second * (1 << fracBits), true);
+            return FixedPoint(first.buf - second * ((T)1 << fracBits), true);
         }
 
         FIXED_POINT_FLOAT_TEMPLATE
         friend FixedPoint operator-(const FP first, const FixedPoint second) {
-            return FixedPoint(first * (1 << fracBits) - second.buf, true);
+            return FixedPoint(first * ((T)1 << fracBits) - second.buf, true);
         }
 
         friend FixedPoint operator*(const FixedPoint first, const FixedPoint second) {
-            return FixedPoint(((TB)first.buf * (TB)(second.buf)) >> fracBits, true);
+            if constexpr(simpleMultiplication)
+                return FixedPoint((first.buf >> (fracBits - (fracBits >> 1))) * (second.buf >> (fracBits >> 1)), true);
+            else
+                return FixedPoint(((TB)first.buf * (TB)(second.buf)) >> fracBits, true);
         }
 
         FIXED_POINT_INTEGRAL_TEMPLATE
@@ -123,12 +125,12 @@ class FixedPoint {
 
         FIXED_POINT_FLOAT_TEMPLATE
         friend FixedPoint operator/(const FixedPoint first, const FP second) {
-            return FixedPoint(((TB)first.buf << fracBits) / (TB)(second * (1 << fracBits)), true);
+            return FixedPoint(((TB)first.buf << fracBits) / (TB)(second * ((T)1 << fracBits)), true);
         }
 
         FIXED_POINT_FLOAT_TEMPLATE
         friend FixedPoint operator/(const FP first, const FixedPoint second) {
-            return FixedPoint((TB)(first * (1 << (2*fracBits))) / (TB)second.buf, true);
+            return FixedPoint((TB)(first * ((T)1 << (2*fracBits))) / (TB)second.buf, true);
         }
 
         FIXED_POINT_INTEGRAL_TEMPLATE
@@ -159,12 +161,12 @@ class FixedPoint {
         FixedPointOperatorsMaker(FixedPoint, first.buf, FixedPoint, second.buf)
         #undef FIXED_POINT_TEMPLATE_DECLARATION
         #define FIXED_POINT_TEMPLATE_DECLARATION FIXED_POINT_INTEGRAL_TEMPLATE
-        FixedPointOperatorsMaker(FixedPoint, first.buf, I, second << fracBits)
+        FixedPointOperatorsMaker(FixedPoint, first.buf, I, (T)second << fracBits)
         FixedPointOperatorsMaker(I, first << fracBits, FixedPoint, second.buf)
         #undef FIXED_POINT_TEMPLATE_DECLARATION
         #define FIXED_POINT_TEMPLATE_DECLARATION FIXED_POINT_FLOAT_TEMPLATE
-        FixedPointOperatorsMaker(FixedPoint, first.buf, FP, second * (1 << fracBits))
-        FixedPointOperatorsMaker(FP, first * (1 << fracBits), FixedPoint, second.buf)
+        FixedPointOperatorsMaker(FixedPoint, first.buf, FP, second * ((T)1 << fracBits))
+        FixedPointOperatorsMaker(FP, first * ((T)1 << fracBits), FixedPoint, second.buf)
         #undef FIXED_POINT_TEMPLATE_DECLARATION
 
         #undef FixedPointOperatorsMaker
@@ -189,7 +191,10 @@ class FixedPoint {
         }
 
         void operator*=(const FixedPoint another) {
-            buf = ((TB)buf * (TB)(another.buf)) >> fracBits;
+            if constexpr(simpleMultiplication)
+                buf = (buf >> (fracBits - (fracBits >> 1))) * (another.buf >> (fracBits >> 1));
+            else
+                buf = ((TB)buf * (TB)(another.buf)) >> fracBits;
         }
 
         FIXED_POINT_INTEGRAL_TEMPLATE
@@ -226,7 +231,7 @@ class FixedPoint {
 
         FIXED_POINT_FLOAT_TEMPLATE
         operator FP() const {
-            constexpr FP k = ((FP)1) / ((FP)(1 << fracBits));
+            constexpr FP k = ((FP)1) / ((FP)((T)1 << fracBits));
             return (FP)buf * k;
         }
 
@@ -252,12 +257,47 @@ class FixedPoint {
         }
 
         T getfrac() {
-            return buf & ((1 << fracBits) - 1);
+            return buf & (((T)1 << fracBits) - 1);
+        }
+
+        unsigned toCharBuf(char* buffer) const {
+            char* wbuf = buffer;
+            T tmpBuf = buf;
+            if (tmpBuf < 0) {
+                *(wbuf++) = '-';
+                tmpBuf = -tmpBuf;
+            }
+            T intPart = tmpBuf >> fracBits;
+            T fracPart = tmpBuf - (intPart << fracBits);
+            char* swapStart = wbuf - 1;
+            do {
+                *(wbuf++) = '0' + (intPart % 10);
+                intPart /= 10;
+            } while (intPart > 0);
+            char* swapEnd = wbuf;
+            while (++swapStart < --swapEnd) {
+                *swapStart ^= *swapEnd;
+                *swapEnd ^= *swapStart;
+                *swapStart ^= *swapEnd;
+            }
+            if (fracPart) {
+                *(wbuf++) = '.';
+                while (fracPart) {
+                    fracPart *= 10;
+                    T digit = fracPart >> fracBits;
+                    fracPart -= digit << fracBits;
+                    *(wbuf++) = '0' + digit;
+                }
+            }
+            return wbuf - buffer;
         }
 
         template<class Stream>
         friend Stream& operator << (Stream& stream, const FixedPoint fp) {
-            stream << (float)fp;
+            char buf[64];
+            unsigned size = fp.toCharBuf(buf);
+            buf[size] = 0;
+            stream << buf;
             return stream;
         }
 
@@ -270,7 +310,7 @@ class FixedPoint {
         }
 
     static_assert(std::is_signed<T>::value == std::is_signed<TB>::value, "T and TB must be both signed or unsigned.");
-    static_assert(sizeof(T) * 8 >= fracBits, "There cannot be more fraction bits that bits in buf type.");
+    static_assert(sizeof(T) * 8 > fracBits, "There must be less fraction bits than bits in buf type.");
     static_assert(sizeof(T) <= sizeof(TB), "Type for multiplication must be equal or greater than buf type.");
 
     #undef FIXED_POINT_INTEGRAL_TEMPLATE
@@ -291,24 +331,20 @@ namespace std {
         return x.getBuf() >> fracBits;
     }
 
-
     template<typename T, typename TB, unsigned fracBits>
     T ceil(const FixedPoint<T, TB, fracBits> x) {
         return (x.getBuf() >> fracBits) + !!x.getfrac();
     }
-
 
     template<typename T, typename TB, unsigned fracBits>
     T round(const FixedPoint<T, TB, fracBits> x) {
         return (x.getBuf() >> fracBits) + (x.getfrac() >> (fracBits-1));
     }
 
-
     template<typename T, typename TB, unsigned fracBits>
     FixedPoint<T, TB, fracBits> abs(const FixedPoint<T, TB, fracBits> x) {
         return x.isNeg() ? -x : x;
     }
-
 
     template<typename T, typename TB, unsigned fracBits>
     FixedPoint<T, TB, fracBits> sign(const FixedPoint<T, TB, fracBits> x) {
